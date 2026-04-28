d4577958e7759d187c81d7d96ac7c18382880973
Robert Olson <olson@mcs.anl.gov>
2026-04-28 15:06:28 -0500

    Remove debug import path.

--- service-scripts/App-Docking.pl
+++ service-scripts/App-Docking.pl
@@ -24,10 +24,8 @@ use File::Basename;
 use File::Temp;
 use JSON::XS;
 use Getopt::Long::Descriptive;
-# use DockingCompute;
-use lib '/home/nbowers/bvbrc-dev/dev_container/modules/bvbrc_docking/lib/';  # Adjust this path if necessary
+use DockingCompute;
 
-use DockingCompute qw(my_function);  # Import the desired function
 my $docking_compute = new DockingCompute();
 
 my $app = Bio::KBase::AppService::AppScript->new(sub { $docking_compute->run(@_); },
